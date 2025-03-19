d7feb7636bee4593850ae9393f57bf2f45c0031f
peyman ka <100542104+Pcuchulu@users.noreply.github.com>
2025-03-19 17:09:10 +0330

    Update page.js

--- co2-prediction-app/app/page.js
+++ co2-prediction-app/app/page.js
@@ -68,7 +68,7 @@ export default function Home() {
   return (
     <div className="container mx-auto p-8 bg-gray-50 rounded-xl shadow-lg">
       <h1 className="text-3xl font-semibold text-center mb-6 text-blue-600">
-        CO₂ Methanation Prediction
+        CarboMethX
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
